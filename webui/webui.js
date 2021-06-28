2043835efa56880224986f2437183010416b5b58
Charles H. Lowery <chucklowery@gmail.com>
2021-06-27 22:05:00 -0600

    Update webui.js

--- webui/webui.js
+++ webui/webui.js
@@ -1,29 +1,29 @@
 var express = require('express');
 var app = express();
-var redis = require('redis');
+//var redis = require('redis');
 
-var client = redis.createClient(6379, 'redis');
-client.on("error", function (err) {
-    console.error("Redis error", err);
-});
+//var client = redis.createClient(6379, 'redis');
+// client.on("error", function (err) {
+//     console.error("Redis error", err);
+// });
 
 app.get('/', function (req, res) {
     res.redirect('/index.html');
 });
 
-app.get('/account', function (req, res) {
-    client.get('account', function (err, account) {
-            var now = Date.now() / 1000;
-            res.json( {
-                "now": now,
-                "account": account
-            });
-    });
-});
+// app.get('/account', function (req, res) {
+//     client.get('account', function (err, account) {
+//             var now = Date.now() / 1000;
+//             res.json( {
+//                 "now": now,
+//                 "account": account
+//             });
+//     });
+// });
 
-app.post('/account', function (req, res) {
-    client.set('account', 300);
-});
+// app.post('/account', function (req, res) {
+//     client.set('account', 300);
+// });
 
 app.use(express.static('files'));
 
